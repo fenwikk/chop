69c99bc5a1e831220908123869dd47c5465a8492
Filip Mårtensson <fenwikk@protonmail.com>
2023-06-13 16:49:45 +0200

    More 4 you

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,56 +10,54 @@ const Home: NextPage = () => {
   };
   const favorites: Favorite[] = [
     {
-      name: "Pizza",
+      name: "Pad Thai",
       description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl eget ultricies ultricies.",
+        "Stekta risnudlar, blandade grönsaker, ägg, krossade jordnötter, gräslök, koriander, böngroddar, soja och chili.",
       price: 99,
-      image: "https://picsum.photos/500/300",
+      image: "/Pad_Thai.jpeg",
     },
     {
-      name: "Pizza",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl eget ultricies ultricies.",
-      price: 99,
-      image: "https://picsum.photos/500/300",
+      name: "Maki Ebi",
+      description: "6 bitars räkor",
+      price: 55,
+      image: "/Maki_Ebi.jpeg",
     },
     {
-      name: "Pizza",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl eget ultricies ultricies.",
+      name: "Panko Chicken",
+      description: "Marinerad kyckling med jordnötssås och ris.",
       price: 99,
-      image: "https://picsum.photos/500/300",
+      image: "/Panko_Chicken.jpeg",
     },
   ];
 
   return (
-    <>
+    <div className="overflow-x-hidden">
       <div className="absolute z-50 w-full">
         <div className="container flex grid-cols-[20%,60%,20%] items-center justify-between py-6 text-white md:grid">
           <Image src="/logo.png" height={56} width={100} alt="Logo" />
           <nav>
-            <div className="hidden items-center justify-center gap-4 font-black uppercase md:flex">
+            <div className="hidden items-center justify-center gap-8 font-black uppercase md:flex">
               <a
                 className="relative after:absolute after:right-0 after:block after:h-[2px] after:w-0 after:bg-white after:duration-200 after:hover:w-full"
-                href=""
+                href="https://thainsushi.qopla.com/restaurant/thai-n-sushi-for-you---rosengard-centrum/q98Mm2PvzA/order"
               >
                 Vår meny
               </a>
               <a
                 className="relative after:absolute after:right-0 after:block after:h-[2px] after:w-0 after:bg-white after:duration-200 after:hover:w-full"
-                href=""
+                href="https://www.foodora.se/restaurant/s7gw/thai-n-sushi-for-you"
               >
                 Beställ
               </a>
               <a
                 className="relative after:absolute after:right-0 after:block after:h-[2px] after:w-0 after:bg-white after:duration-200 after:hover:w-full"
-                href=""
+                href="https://goo.gl/maps/TkaPMBSvaadxNx5YA"
               >
                 Hitta hit
               </a>
               <a
                 className="relative after:absolute after:right-0 after:block after:h-[2px] after:w-0 after:bg-white after:duration-200 after:hover:w-full"
-                href=""
+                href="mailto:filip.martensson@seabird.digital"
               >
                 Kontakt
               </a>
@@ -67,7 +65,10 @@ const Home: NextPage = () => {
           </nav>
           <div className="flex justify-end">
             <button className="w-fit rounded-full border-none bg-ts-red px-3 py-2">
-              (0)12-345 67 89
+              <a href="tel:040930630">
+                <span className="hidden md:block">(0)40-93 06 30</span>
+                <span className="md:hidden">Ring hit</span>
+              </a>
             </button>
           </div>
         </div>
@@ -87,22 +88,23 @@ const Home: NextPage = () => {
         </div>
 
         <div className="container flex flex-col items-center py-12">
-          <h2 className="text-4xl font-black text-blue-950">Våra favoriter</h2>
-          <div className="my-6 grid grid-rows-3 gap-6 sm:grid-cols-3 sm:grid-rows-none">
+          <h2 className="text-4xl font-black text-ts-blue">Våra favoriter</h2>
+          <div className="my-6 grid grid-rows-3 gap-6 lg:grid-cols-3 lg:grid-rows-none">
             {favorites.map((favorite) => (
               <div
                 key={favorite.name}
-                className="relative flex flex-col items-center overflow-hidden rounded-lg bg-blue-950 text-white"
+                className="relative flex w-full flex-col items-center overflow-hidden rounded-lg bg-ts-blue text-white"
               >
-                <span className="absolute right-3 top-2 text-2xl font-black text-ts-red">
+                <span className="absolute right-1 top-1 rounded-xl bg-ts-red p-2 text-2xl font-black">
                   {favorite.price} kr
                 </span>
                 <img
                   src={favorite.image}
                   alt={favorite.name}
-                  className="max-h-40 w-full object-cover"
+                  className="w-full object-cover lg:max-h-52 xl:max-h-60"
                 />
-                <div className="m-4">
+
+                <div className="absolute bottom-0 w-full bg-ts-blue p-4 lg:static">
                   <h3 className="font-black">{favorite.name}</h3>
                   <p>{favorite.description}</p>
                 </div>
@@ -111,7 +113,9 @@ const Home: NextPage = () => {
           </div>
 
           <button className="w-fit rounded-full border-none bg-ts-red px-5 py-3 text-lg text-white">
-            Se hela menyn
+            <a href="https://thainsushi.qopla.com/restaurant/thai-n-sushi-for-you---rosengard-centrum/q98Mm2PvzA/order">
+              Se hela menyn
+            </a>
           </button>
         </div>
 
@@ -120,49 +124,57 @@ const Home: NextPage = () => {
             <div className="flex justify-end gap-2">
               <div>
                 <Image
-                  src="https://picsum.photos/40/40"
+                  src="/kicki.png"
                   width={40}
                   height={40}
-                  alt="jörgen gustafson"
-                  className="aspect-square rounded-full"
+                  alt="Kicki Szabo"
                 />
               </div>
               <div>
-                <h3>Jörgen Gustafson</h3>
-                <p className="text-sm">Google Maps</p>
+                <h3>Kicki Szabo</h3>
+                <p className="text-xs">Local Guide, Google Maps</p>
               </div>
             </div>
-            <p className="text-2xl font-black">
-              Bästa pizzan i stan!
-              <br />
-              Utan tvekan.
+            <p className="max-w-md text-xl font-bold">
+              Trevligt bemötande, tipsade och rekommenderade, då jag inte kände
+              till menyn. Noga allergi info och hantering av maten.
             </p>
           </div>
         </div>
 
-        <footer className="bg-blue-950 py-12 text-white">
-          <div className="container flex max-w-[75%] items-center justify-between">
-            <div className="flex flex-col">
-              <a href="">Meny</a>
-              <a href="">Beställ</a>
-              <a href="">Kontakt</a>
-            </div>
+        <footer className="bg-ts-blue  text-white">
+          <div className="bg-black/30 py-12">
+            <div className="container flex max-w-[75%] items-center justify-between">
+              <div className="flex flex-col">
+                <a href="https://thainsushi.qopla.com/restaurant/thai-n-sushi-for-you---rosengard-centrum/q98Mm2PvzA/order">
+                  Meny
+                </a>
+                <a href="https://www.foodora.se/restaurant/s7gw/thai-n-sushi-for-you">
+                  Beställ
+                </a>
+                <a href="https://goo.gl/maps/TkaPMBSvaadxNx5YA">Hitta Hit</a>
+                <a href="mailto:filip.martensson@seabird.digital">Kontakt</a>
+              </div>
 
-            <div className="text-right">
-              Copyright 2023 &copy; Seabird Digital.
-              <br />
-              All rights reserved.
+              <div className="text-right">
+                Copyright 2023 &copy; Seabird Digital.
+                <br />
+                All rights reserved.
+              </div>
             </div>
           </div>
         </footer>
       </main>
 
       <div className="h-14 md:h-auto">
-        <a className="fixed bottom-0 right-0 z-50 flex w-full justify-center bg-ts-red px-4 py-2 hover:cursor-pointer hover:opacity-80 md:bottom-1/2 md:-m-20 md:w-auto md:-rotate-90 md:duration-200">
+        <a
+          href="https://www.foodora.se/restaurant/s7gw/thai-n-sushi-for-you"
+          className="fixed bottom-0 right-0 z-50 flex w-full justify-center bg-ts-red px-4 py-2 hover:cursor-pointer hover:opacity-80 md:bottom-1/2 md:-m-20 md:w-auto md:-rotate-90 md:duration-200"
+        >
           <span className="text-4xl font-black text-white">beställ nu</span>
         </a>
       </div>
-    </>
+    </div>
   );
 };
 
